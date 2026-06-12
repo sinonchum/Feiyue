8ba3d5f7af47f556092d012e84807dca3208db91
sinonchum <f.simon.qin@gmail.com>
2026-06-12 18:41:31 +0200

    feat: add curation review gate

--- packages/feiyue-core/feiyue_core/curation/__init__.py
+++ packages/feiyue-core/feiyue_core/curation/__init__.py
@@ -8,12 +8,22 @@ from feiyue_core.curation.distillation_proposal import (
     ProposalStatus,
     build_distillation_proposal,
 )
+from feiyue_core.curation.review_gate import (
+    InvalidReviewTransitionError,
+    ReviewDecision,
+    ReviewDecisionStatus,
+    ReviewGate,
+)
 
 __all__ = [
     "ALLOWED_ASSET_TYPES",
     "CuratorInput",
     "DistillationProposal",
+    "InvalidReviewTransitionError",
     "ProposalPatch",
     "ProposalStatus",
+    "ReviewDecision",
+    "ReviewDecisionStatus",
+    "ReviewGate",
     "build_distillation_proposal",
 ]
