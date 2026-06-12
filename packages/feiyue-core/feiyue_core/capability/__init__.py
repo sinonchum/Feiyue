dc437ef95d5c06a261c2cb121a858bf23e1fd43d
sinonchum <f.simon.qin@gmail.com>
2026-06-12 19:04:04 +0200

    feat: add worker performance records

---
+++ packages/feiyue-core/feiyue_core/capability/__init__.py
@@ -0,0 +1,8 @@
+"""Capability contracts for Feiyue workers."""
+
+from feiyue_core.capability.performance_record import (
+    WorkerPerformanceRecord,
+    WorkerTaskResult,
+)
+
+__all__ = ["WorkerPerformanceRecord", "WorkerTaskResult"]
